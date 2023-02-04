d28c6373946887a67dc642896f63d171421c2429
mrc <1552397900@qq.com>
2023-02-04 16:08:26 +0800

    文件大小写问题

---
+++ src/typeFile/typeFile.cpp
@@ -0,0 +1,17 @@
+﻿#include "typeFile.h"
+#include "typeText.h"
+#include "typeXml.h"
+#include <QFile>
+#include <QDir>
+
+QSharedPointer<typeFile::Text> typeFile::generate::file::textInstance(const QString& fileExisPath) {
+	return QSharedPointer<Text>( new Text( fileExisPath ) );
+}
+
+QSharedPointer<typeFile::Xml> typeFile::generate::file::xmlInstance(const QString& fileExisPath) {
+	return QSharedPointer<Xml>( new Xml( fileExisPath ) );
+}
+
+QSharedPointer<typeFile::XmlBuff> typeFile::generate::obj::xmlNodeInstance(const QString& str) {
+	return nullptr;
+}
